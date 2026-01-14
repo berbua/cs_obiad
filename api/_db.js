02fda39d79f5ac9a1c49de72f36ea81eed2e32a2
Katarzyna Berbeka <katarzyna.berbeka@creativestyle.pl>
2026-01-14 15:15:37 +0100

    Auto-select database: Postgres on Vercel, JSON locally

--- api/_db.js
+++ api/_db.js
@@ -1,2 +1,7 @@
-// Vercel Postgres database
-module.exports = require('../backend/database-postgres');
+// Auto-select database based on environment
+// Use Postgres on Vercel (production), JSON locally
+if (process.env.VERCEL || process.env.OBIAD_POSTGRES_URL) {
+  module.exports = require('../backend/database-postgres');
+} else {
+  module.exports = require('../backend/database-json');
+}
